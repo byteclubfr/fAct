778e7759d6b8d0eba4f39ecc8aaa228a193f3525
Nicolas Chambrier <naholyr@gmail.com>
2014-07-31 11:07:50 +0200

    Auth: google

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -38,7 +38,7 @@ angular.module('fAct.app', [
       users: ['nicolas', 'lilian', 'thomas']
     },
     loginMessage: "Veuillez vous identifier pour accéder à cette application\n(cliquer sur OK pour continuer)",
-    loginProvider: "persona"
+    loginProvider: "google"
   }
 })
 
